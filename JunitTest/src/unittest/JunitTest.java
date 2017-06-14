9409ffb95777a9370f624d9435b5cf7438ef7535
arunram <arunram@PB119AT70N.Etechaces.com>
2017-06-14 17:02:31 +0530

    First commit

---
+++ JunitTest/src/unittest/JunitTest.java
@@ -0,0 +1,14 @@
+package unittest;
+
+import static org.junit.Assert.assertEquals;
+
+public class JunitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="arun";
+	      assertEquals("Junit",str);
+
+	}
+
+}
